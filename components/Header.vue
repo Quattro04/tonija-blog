b1581a5e60a7029df693a7943254c5da7d13c124
matija <matija.jeras64@gmail.com>
2021-03-20 14:44:05 +0100

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="flex bg-green-300 h-16 flex items-center fixed w-full">
+        <span class="text text-3xl text-white mx-auto">helpful tips</span>
+    </nav>
+</template>
+
+<style scoped>
+    .text {
+        font-family: 'Leckerli One', cursive;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container flex flex-col p-10 pt-24">
+        <h1 class="title font-medium mb-5">
+            Top 10 ways on how to make money online for beginners 
+        </h1>
+        <div class="flex flex-col">
+            <p class="mb-3">Making money is the best hobby. And, knowing how to make money online for beginners is the sweetest knowledge on earth. Maybe you are tired of finding ways to earn money, especially in this Covid-19 pandemic situation. How about earning money without going out of your home? Sounds impossible! But you can make this a game-changing moment if you know how to make money online for beginners.</p>
+            <p class="mb-3">How to make money online for beginners? The digital era has ushered almost limitless opportunities on us. It has also changed the age-old perception of linking money with a 9 to 5 job.  Now, one can explore the internet for all kinds of information and use that to their advantage.</p>
+            <p class="mb-8 font-medium">Making money online is not that difficult as you may think. Let’s check the top 10 ways to make money online fast.</p>
+            <h2 class="mb-3 text-xl">1. Freelance Writing</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/1.jpg" /></div>
+            <p class="mb-8">When looking for how to make money online without paying anything freelance writing is the simplest you to do that. It is ideal for those who have creativity and skills of writing. You are free to do this anytime and from anywhere you like. You have only to find a niche that is comfortable to you and start writing. As because there is always a demand for good content, finding jobs become easy. One has only to create a freelancers account with sites like Upwork, Fiverr, Freelancer, iWriter, etc, and become a freelancer. It is also one of the best ways of how to make money online as a teenager.</p>
+            <h2 class="mb-3 text-xl">2. Start Dropshipping</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/2.jpg" /></div>
+            <p class="mb-5">Starting a dropshipping business is one of the most popular ways of how to make money online for beginners. Its rapid growth ensures its viability to make money online from home. It is a unique retail business model following which a store never keeps stock of the products it sells. When there is a requirement, the business buys it from a third party. And, this third party delivers the product to the customer directly. One has to purchase a domain and needs to maintain a Shopify account. You can find tons of blogs and articles on the web about starting a dropshipping business.</p>
+            <h2 class="mb-3 text-xl">3. Online tutoring</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/3.jpg" /></div>
+            <p class="mb-5">When looking for how to make money online for free, online tutoring is one of the great ways to do that. Online tutoring is gaining much importance in this post-pandemic situation. If you have expertise in any particular subject, it can be an ideal venture as a beginner. It will also give you a flexible lifestyle, and you can do this almost anywhere in the world. Your charges will depend on the niche you choose and your experience. You can also join TutorMe for tutoring jobs.</p>
+            <h2 class="mb-3 text-xl">4. Write reviews</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/4.png" /></div>
+            <p class="mb-5">Consumers always read reviews. Reviews also carry the same weight as personal recommendations. It is why plenty of organizations pay reviewers as a part of their online marketing strategy. So, writing reviews of a product or service is one of the best ways of how to make money online for beginners. You may join FIVERR for writing reviews for products on Amazon.</p>
+            <h2 class="mb-3 text-xl">5. Affiliate marketing</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/5.jpg" /></div>
+            <p class="mb-5">Affiliate marketing happens to be the easiest way to make money online. Big business houses use this for promoting their products and services. Anyone can become an affiliate partner. Once you become an affiliate partner, you have to promote their business through your website. You can also do this using social media platforms. When someone buys their product or uses their services from your referral, you earn a commission. It is a good side hustle.</p>
+            <h2 class="mb-3 text-xl">6. Blogging</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/6.png" /></div>
+            <p class="mb-5">If you have the skill to pen down your thoughts, blogging could be one of the best methods of how to make money online for beginners.  It is easy to do. Just find your favorite niche, a domain, and good hosting. You can start your blogging with WordPress and activate Google Adsense for showing ads on the blogging site. Using use affiliate links on your blogging site will help you earn money online.</p>
+            <h2 class="mb-3 text-xl">7. YouTube Channel</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/7.jpg" /></div>
+            <p class="mb-5">Opening a YouTube channel is also a great way to make money online at home for beginners. You can start a YouTube channel practically on anything from cooking to DIY installation. To start with, you must focus on a single niche. It helps to build a strong and loyal audience. YouTube works like a blog. You may start uploading videos and ads to monetize the YouTube channel. Your income grows with the viewers.</p>
+            <h2 class="mb-3 text-xl">8. Sell photos</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/8.jpg" /></div>
+            <p class="mb-5">If you are looking for the best way to make money online as a beginner, selling photos could help. It is just like opening a YouTube channel. You must have the creativity and be able to harness that for a side hustle to start. Plenty of stock photography websites are there covering every imaginable subject. You may upload photos to these websites for business, designers, magazines, etc to buy them.</p>
+            <h2 class="mb-3 text-xl">9. Virtual Assistance</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/9.png" /></div>
+            <p class="mb-5">Becoming a virtual assistant is another way of how to make money online for beginners.  With a plethora of entrepreneurs crowding the internet, the demand for virtual assistants is experiencing steady growth. Business organizations are now hiring virtual assistants for answering calls, handling mails, processing orders, social media presence, etc. You need to be active on Twitter, Linkedin for reaching brands and finding new clients.</p>
+            <h2 class="mb-3 text-xl">10. Do transcription jobs</h2>
+            <div class="mb-3"><img class="rounded-xl" src="/10.jpg" /></div>
+            <p class="mb-5">If you can type fast and are strong in English, doing transcription jobs from home could be the way of how to make money online fast. It involves converting audio files into texts. Several websites offer such jobs. You can join Rev for this. Sign up for getting approval for transcription. Once approved, you can choose from the available transcription jobs and transcribe that. They make weekly payments through PayPal.</p>
+            <h2 class="mb-3 text-xl">Parting words</h2>
+            <p class="mb-5">ll the above of how to make money online for beginners may not be for you. Pick one that suits you, and give it a shot to start earning money. While these will not give you a fortune overnight, they can help generate a steady income.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    max-width: 768px;
+}
+
+.title {
+    font-family:
+        'Quicksand',
+        'Source Sans Pro',
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        sans-serif;
+    font-size: 35px;
+    color: #35495e;
+}
+
+.subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+}
+</style>
